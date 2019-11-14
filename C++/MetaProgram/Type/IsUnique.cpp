03b9b0136df5f64e2890987302ba5a256a6224c2
wintel2014 <wintel@dot.com>
2019-11-14 09:58:09 +0000

    Update unique meta program

--- C++/MetaProgram/Type/IsUnique.cpp
+++ C++/MetaProgram/Type/IsUnique.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <type_traits>
 
 namespace check_first {
 template <typename... Args>
-struct is_unique_type;
+struct is_unique_type
+{
+    constexpr static bool value = true;
+};
 
 template <typename A, typename B, typename... Args>
 struct is_unique_type<A, B, Args...>
 {
-    constexpr static bool value = is_unique_type<A,B>::value && is_unique_type<A, Args...>::value;
+    constexpr static bool value = (is_unique_type<A,B>::value && is_unique_type<A, Args...>::value);
 };
 
 template <typename A, typename B>
@@ -23,14 +27,34 @@ struct is_unique_type<A,A>
 };
 }
 
-template <typename A, typename B, typename... Args>
+template <typename... Args>
 struct is_unique_type
 {
-    constexpr static bool value = check_first::is_unique_type<A, B, Args...>::value && check_first::is_unique_type<B, Args...>::value;
+    constexpr static bool value = true;
+};
+//is_unqique_type<A...> guarantee that [A...] is unique
+//If B can't be found in [A...] , then [B, A...] is unique
+template <typename A, typename... Args>
+struct is_unique_type<A,Args...>
+{
+    constexpr static bool value = check_first::is_unique_type<A, Args...>::value && is_unique_type<Args...>::value;
 };
 
+template <size_t... Args>
+struct is_unique_integer
+{
+    constexpr static bool value = is_unique_type<std::integral_constant<size_t, Args>...>::value;
+};
+
+template <size_t v>
+using integral = std::integral_constant<size_t, v>;
 int main()
 {
-    std::cout<<is_unique_type<int, long, double, long, short>::value<<"\n";
-    std::cout<<is_unique_type<int, long, double, short>::value<<"\n";
+    static_assert(is_unique_type<int, double, long, long>::value==false, "");
+    static_assert(is_unique_type<int, long, double, short>::value, "");
+    static_assert(!is_unique_type<integral<1>, integral<2>, integral<3>, integral<4>, integral<5>, integral<5> ,integral<6>, integral<7>>::value, "Not uniqure");
+
+    std::cout<<is_unique_integer<1, 3, 2, 2>::value<<"\n";
+    std::cout<<is_unique_integer<1, 2, 3,4,5,6,7,8,9,0,6>::value<<"\n";
+    std::cout<<is_unique_integer<1, 2, 3,4,5,6,7,8,9,0,10,11,12,13,14,15,16>::value<<"\n";
 }
