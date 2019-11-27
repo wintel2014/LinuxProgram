eac16a0e6f247c424f8dab4d9fafd8a58d36c07e
wintel2014 <wintel@dot.com>
2019-11-27 01:12:42 +0000

    The unresolved symbols must be processed before the provider so, they are can be put into "unresolved set"

---
+++ Compile_Link/link_unresolved_symbol/integer_literal.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+    double ret = sin(5);
+    printf("%f\n", ret);
+}
---
+++ Compile_Link/link_unresolved_symbol/variable.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+    double var = argc;
+    double ret = sin(var);
+    printf("%f\n", ret);
+}
