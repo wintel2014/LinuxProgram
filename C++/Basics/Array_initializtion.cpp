b1165333db3e433d61be70fdbab1cfe54be9abd1
simon <simon@github.com>
2023-02-20 17:30:08 +0800

    Array initialization test

--- C++/Basics/Array_initializtion.cpp
+++ C++/Basics/Array_initializtion.cpp
@@ -1,9 +1,11 @@
+#include <stdio.h>
 struct FOO{
     int a;
     int b;
     int c;
 };
 
+char gVar[1024*1024] {'a'};
 int main()
 {
     FOO foo1 = {a:1, b:2};
@@ -18,6 +20,13 @@ int main()
     
     //constexpr int MAX=12;
     //int a[MAX] = {1, 3, 5, 7, 9, [MAX-5] = 8, 6, 4, 2, 0};
+
+    static char var[1024*1024] {'a'};
+    static char bss_var[1024*1024];
+    //static char var[1024*1024]  = {'a'};
+    printf("%p %c-%c-\n", var, var[0], var[1024*1024-2]);                 //0x504060 a--
+    printf("%p %c-%c-\n", gVar, gVar[0], gVar[1024*1024-2]);              //0x404060 a--
+    printf("%p %c-%c-\n",  bss_var,  bss_var[0], bss_var[1024*1024-2]);   //0x604080 --
    
 }
     
